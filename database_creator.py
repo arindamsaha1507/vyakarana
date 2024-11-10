ab83b297652c779da5c93fd6c6be275df5f6d5e5
Arindam Saha <arindamsaha1507@gmail.com>
2024-11-10 12:33:45 +0000

    Added Collected all verbs

--- database_creator.py
+++ database_creator.py
@@ -1,10 +1,49 @@
 """Module to create all the tables in the database."""
 
+import multiprocessing
 import json
 import sqlite3
 import pathlib
 
 import pandas as pd
+
+
+LAKARA_DICT = {
+    "lat": "लट्",
+    "lit": "लिट्",
+    "lut": "लुट्",
+    "lrut": "लृट्",
+    "lot": "लोट्",
+    "lang": "लङ्",
+    "vidhiling": "विधिलिङ्",
+    "let": "लेट्",
+    "ashirling": "आशीर्लिङ्",
+    "lung": "लुङ्",
+    "lrung": "लृङ्",
+}
+
+PRATYAYA_DICT = {
+    "yangluk": "यङ्लुक्",
+    "san": "सन्",
+    "yang": "यङ्",
+    "nich": "णिच्",
+    "yak": "यक्",
+    "-": "-",
+}
+
+PURUSHA_DICT = {
+    "१": "प्रथमपुरुष",
+    "२": "मध्यमपुरुष",
+    "३": "उत्तमपुरुष",
+}
+
+VACHANA_DICT = {
+    "१": "एकवचन",
+    "२": "द्विवचन",
+    "३": "बहुवचन",
+}
+
+PADA_DICT = {"a": "आत्मनेपदी", "p": "परस्मैपदी"}
 
 
 def initiate_database() -> sqlite3.Connection:
@@ -24,7 +63,6 @@ def create_dhatu_table(conn: sqlite3.Connection):
         data = json.load(f)["data"]
 
     df = pd.DataFrame(data)
-    print(df.columns)
 
     # This is a complex column, we will handle it later
     df = df.drop(columns=["upasargas"])
@@ -32,19 +70,27 @@ def create_dhatu_table(conn: sqlite3.Connection):
     df.to_sql("dhatu", conn, if_exists="replace", index=False)
 
 
-def create_dhaturupa(conn: sqlite3.Connection):
+def create_dhaturupa(filename: pathlib.Path):
     """Create the dhaturupa table."""
 
-    json_file = pathlib.Path("dhatu/dhatuforms.txt")
+    print(f"Creating table for {filename}")
 
-    with json_file.open(encoding="utf-8") as f:
+    conn = sqlite3.connect("database.db")
+
+    with filename.open(encoding="utf-8") as f:
         data: dict[str, dict] = json.load(f)
 
-    lakaras = data["01.0001"].keys()
+    basic_lakaras = list(data["01.0001"].keys())
+    basic_lakaras = [lakara[1:] for lakara in basic_lakaras]
+
+    lakaras = ["a" + lakara for lakara in basic_lakaras]
+    lakaras += ["p" + lakara for lakara in basic_lakaras]
 
     for lakara in lakaras:
-        table_name = f"dhaturupa_{lakara}"
+        table_name = filename.stem + "_" + lakara
         create_dhaturupa_table(data, conn, table_name, lakara)
+
+    conn.close()
 
 
 def create_dhaturupa_table(
@@ -55,52 +101,147 @@ def create_dhaturupa_table(
     df = pd.DataFrame(
         columns=[
             "baseindex",
-            "prathamapurusasingular",
-            "prathamapurusadual",
-            "prathamapurusaplural",
-            "madhyamapurusasingular",
-            "madhyamapurusadual",
-            "madhyamapurusaplural",
-            "uttamapurusasingular",
-            "uttamapurusadual",
-            "uttamapurusaplural",
+            "१.१",
+            "१.२",
+            "१.३",
+            "२.१",
+            "२.२",
+            "२.३",
+            "३.१",
+            "३.२",
+            "३.३",
         ]
     )
     for key, value in data.items():
-        # print(key, value)
+
+        if lakara not in value:
+            continue
+
         forms = value[lakara]
+
         if not forms:
             continue
 
         rupas = forms.split(";")
 
         if len(rupas) != 9:
-            raise ValueError(f"Error in {key}: {forms}")
+            raise ValueError(
+                f"Error in {key}: {forms}, len: {len(rupas)}, content: {rupas}"
+            )
 
         row = {
             "baseindex": key,
-            "prathamapurusasingular": rupas[0],
-            "prathamapurusadual": rupas[1],
-            "prathamapurusaplural": rupas[2],
-            "madhyamapurusasingular": rupas[3],
-            "madhyamapurusadual": rupas[4],
-            "madhyamapurusaplural": rupas[5],
-            "uttamapurusasingular": rupas[6],
-            "uttamapurusadual": rupas[7],
-            "uttamapurusaplural": rupas[8],
+            "१.१": rupas[0],
+            "१.२": rupas[1],
+            "१.३": rupas[2],
+            "२.१": rupas[3],
+            "२.२": rupas[4],
+            "२.३": rupas[5],
+            "३.१": rupas[6],
+            "३.२": rupas[7],
+            "३.३": rupas[8],
         }
 
         df.loc[len(df)] = row
 
     df.to_sql(table_name, conn, if_exists="replace", index=False)
+    print(f"Created table {table_name}")
+
+
+def create_all_dhatu_related_tables():
+    """Create all the tables related to dhatu."""
+
+    conn = initiate_database()
+    create_dhatu_table(conn)
+    conn.close()
+
+    files = pathlib.Path("dhatu").glob("*.txt")
+    files = [
+        file
+        for file in files
+        if file.name not in ["data.txt", "dhatuforms_krut.txt", "dhatuprayogas.txt"]
+    ]
+
+    with multiprocessing.Pool() as pool:
+        pool.starmap(create_dhaturupa, [(file,) for file in files])
+
+
+def collect_all_verbs():
+    """Collect all the verbs from the dhaturupa tables."""
+
+    conn = sqlite3.connect("database.db")
+
+    tables = conn.execute(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name LIKE 'dhatu_%'"
+    ).fetchall()
+
+    tables = [table[0] for table in tables]
+
+    cursor = conn.cursor()
+
+    cursor.execute("DROP TABLE IF EXISTS verbs")
+
+    cursor.execute(
+        "CREATE TABLE verbs (verb TEXT, baseindex TEXT, lakara TEXT, pada TEXT, pratyaya TEXT, purusha TEXT, vachana TEXT)"
+    )
+
+    for index, table in enumerate(tables):
+        print(f"Collecting verbs from {index}/{len(tables)}: {table}")
+
+        df = pd.read_sql(f"SELECT * FROM {table}", conn)
+
+        for _, row in df.iterrows():
+            for column in df.columns[1:]:
+                if row[column]:
+
+                    for word in row[column].split(","):
+
+                        purusha, vachana = column.split(".")
+
+                        parts = table.split("_")
+
+                        lakara = parts[-1]
+                        pada = lakara[0]
+                        lakara = lakara[1:]
+
+                        if len(parts) == 3:
+                            pratyaya = parts[1]
+                        else:
+                            pratyaya = "-"
+
+                        cursor.execute(
+                            "INSERT INTO verbs VALUES (?, ?, ?, ?, ?, ?, ?)",
+                            (
+                                word,
+                                row["baseindex"],
+                                LAKARA_DICT[lakara],
+                                PADA_DICT[pada],
+                                PRATYAYA_DICT[pratyaya],
+                                PURUSHA_DICT[purusha],
+                                VACHANA_DICT[vachana],
+                            ),
+                        )
+
+    conn.commit()
+
+    print("Finished collecting verbs")
+
+    # print("Sample verbs:")
+    # for row in conn.execute("SELECT * FROM verbs LIMIT 10"):
+    #     print(row)
+
+    lakaras = conn.execute("SELECT DISTINCT lakara FROM verbs").fetchall()
+    print("Lakaras:", lakaras)
+    pratyayas = conn.execute("SELECT DISTINCT pratyaya FROM verbs").fetchall()
+    print("Pratyayas:", pratyayas)
+
+    conn.close()
 
 
 def main():
-    """Main function to create the database."""
-    conn = initiate_database()
-    create_dhatu_table(conn)
-    create_dhaturupa(conn)
-    conn.close()
+    """Main function to create all the tables in the database."""
+    # create_all_dhatu_related_tables()
+    # collect_all_verbs()
 
 
 if __name__ == "__main__":
